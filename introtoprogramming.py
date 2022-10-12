faf34c18229810d2b21b30d9f53169665375a082
cynthialmcginnis <54970966+cynthialmcginnis@users.noreply.github.com>
2022-10-12 16:46:18 +0900

    Add files via upload

---
+++ introtoprogramming.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("Welcome to Introduction to Programing!")
+
+
+# In[8]:
+
+
+programming = input("Do you have programming experience? type yes or no\n")
+
+
+# In[9]:
+
+
+if programming == 'no':
+    print("No problem!  You'll do great")
+else:
+    print("Cool!")
+
+
+# In[ ]:
+
+
+
+
